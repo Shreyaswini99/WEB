ff6f719c489991857c80882d05b63eaf5ad39fc1
Shreyaswini99 <53867683+Shreyaswini99@users.noreply.github.com>
2021-01-04 16:05:29 +0530

    Update Lab P7.php

--- Programs/Lab P7.php
+++ Programs/Lab P7.php
@@ -1,3 +1,7 @@
+<!-- 
+Name:Shreyawini
+Usn:4MT17IS048
+7. Write a PHP program to display a digital clock which displays the current time of the server. -->
 <strong style="color:green;">
 <?php
 date_default_timezone_set('Asia/Calcutta');
